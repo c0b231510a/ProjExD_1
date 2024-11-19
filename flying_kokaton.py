0904c1740c5011a02374ad07a0dc219cfce4846e
ryuu <c0b231510a@edu.teu.ac.jp>
2024-11-19 17:41:42 +0900

    演習問題

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -9,13 +9,12 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.transform.scale(bg_img, (1600, 900))
     bg_flip_img = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
 
     bg_x = 0
-    bg_width = bg_img.get_width()
     kk_rect = kk_img.get_rect()
     kk_rect.center = (300, 200)
 
@@ -23,23 +22,27 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+
         bg_x -= 2
-        if bg_x <= -bg_width:
-            bg_x += bg_width
+        if bg_x <= -1600:
+            bg_x += 1600
+
         screen.blit(bg_img, [bg_x, 0])
-        screen.blit(bg_flip_img, [bg_x + bg_width, 0])
-        screen.blit(bg_img, [bg_x - bg_width, 0])
+        screen.blit(bg_flip_img, [bg_x + 1600, 0])
+        screen.blit(bg_img, [bg_x - 1600, 0])
         screen.blit(bg_flip_img, [bg_x, 0])
 
         keys = pg.key.get_pressed()
+        dx, dy = 0, 0
         if keys[pg.K_UP]:
-            kk_rect.move_ip(0, -1)
+            dy -= 1
         if keys[pg.K_DOWN]:
-            kk_rect.move_ip(0, 1)
+            dy += 1
         if keys[pg.K_LEFT]:
-            kk_rect.move_ip(-1, 0)
+            dx -= 1
         if keys[pg.K_RIGHT]:
-            kk_rect.move_ip(1, 0)
+            dx += 1
+        kk_rect.move_ip(dx, dy)
 
         screen.blit(kk_img, kk_rect)
 
